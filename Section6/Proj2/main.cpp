9cb8808e25952a0b5b4b6786108db4328b49afe6
Trong_Nguyen <dvnguyentrong96@gmail.com>
2022-12-21 15:55:12 +0700

    trong_update-new-proj

---
+++ Section6/Proj2/main.cpp
@@ -0,0 +1,15 @@
+//Global and local variables
+
+#include <iostream>
+
+using namespace std;
+
+int age{18}; //global variables
+
+
+int main(){
+
+    int age{16}; //local variables
+    cout << age <<endl;
+    return 0;
+}
